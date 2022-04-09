8ee5099051397239f3fa0c8a88c7ad403ce76cee
Chexiz1 <mskapp@mail.ru>
2022-04-09 14:30:13 +0300

    test

---
+++ src/components/PostForm.vue
@@ -0,0 +1,60 @@
+<template>
+  <div>
+    <form @submit.prevent>
+      <h4>Создание поста</h4>
+      <input
+        v-model="post.title"
+        class="input"
+        type="text"
+        placeholder="Название"
+      />
+      <input
+        v-model="post.body"
+        class="input"
+        type="text"
+        placeholder="Описание"
+      />
+      <my-button class="btn" @click="createPost" style="align-self: flex-end"
+        >Создать пост</my-button
+      >
+    </form>
+  </div>
+</template>
+
+<script>
+import MyButton from '@/components/UI/MyButton'
+export default {
+  components: { MyButton },
+  data() {
+    return {
+      post: {
+        title: '',
+        body: '',
+      },
+    }
+  },
+  methods: {
+    createPost() {
+      this.post.id = Date.now()
+      this.$emit('create', this.post)
+      this.post = {
+        title: '',
+        body: '',
+      }
+    },
+  },
+}
+</script>
+
+<style scoped>
+form {
+  display: flex;
+  flex-direction: column;
+}
+.input {
+  width: 100%;
+  border: 1px solid teal;
+  padding: 10px 15px;
+  margin-top: 15px;
+}
+</style>
